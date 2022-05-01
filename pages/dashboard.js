da18e7187a9872f29ee97508e9868f7102990262
Ayodeji <drealayodeji@icloud.com>
2022-05-01 21:09:00 +0100

    Added lists

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,8 +2,11 @@
 import { useContext, useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { AuthContext } from "@/context/AuthContext";
-import { BiPowerOff, BiPlus } from "react-icons/bi";
-import { useToast, Spinner } from "@chakra-ui/react";
+import { BiPowerOff, BiPlus, BiEditAlt } from "react-icons/bi";
+import { FiRefreshCw } from "react-icons/fi";
+import { IoCopyOutline } from "react-icons/io5";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { useToast, Spinner, Box } from "@chakra-ui/react";
 import Meta from "@/defaults/Meta";
 import Link from "next/link";
 import axios from "axios";
@@ -69,6 +72,8 @@ const Dashboard = () => {
   if (!User || !Token || Status.Loading) {
     return (
       <div className="w-screen h-[90vh] flex items-center justify-center">
+        <Meta title={`${User.username.toUpperCase()}'s Dashboard`} />
+
         <Spinner size="xl" />
       </div>
     );
@@ -88,22 +93,30 @@ const Dashboard = () => {
   return (
     <>
       <Meta title={`${User.username.toUpperCase()}'s Dashboard`} />
-      <nav className="flex justify-between items-center py-4 px-5 lg:px-10 bg-white drop-shadow-md">
+      <nav className="fixed top-0 w-screen flex justify-between items-center py-4 px-5 lg:px-10 bg-white drop-shadow-md">
         <img
           className="h-8 lg:h-9 w-auto "
           src="/images/Logo-Black.svg"
           alt="Logo"
         />
 
-        <div className="text-red-600 bg-red-100 p-2 rounded-full cursor-pointer">
-          <Link href="/auth/logout" passHref>
-            <BiPowerOff size="19" />
-          </Link>
+        <div className="flex space-x-4">
+          <button
+            className="text-neutral-800 p-2 rounded-full cursor-pointer"
+            onClick={getData}
+          >
+            <FiRefreshCw size="16" />
+          </button>
+          <div className="text-red-600 bg-red-100 p-2 rounded-full cursor-pointer">
+            <Link href="/auth/logout" passHref>
+              <BiPowerOff size="19" />
+            </Link>
+          </div>
         </div>
       </nav>
 
-      <main className="container w-[90%] md:w-[80%] lg:w-3/6 2xl:w-3/5 mx-auto mt-[5vh] lg:mt-[10vh]">
-        <div className=" mx-auto px-5 py-7 bg-white shadow-md rounded-xl">
+      <main className="container w-[90%] md:w-[80%] lg:w-3/6 2xl:w-3/5 mx-auto mt-[12vh] lg:mt-[15vh]">
+        <div className=" mx-auto px-5 py-7 bg-neutral-50 shadow-md rounded-xl">
           <h1 className="text-primary text-3xl font-semibold">
             <span className="text-neutral-800 font-semibold">@</span>
             {User.username}
@@ -140,8 +153,66 @@ const Dashboard = () => {
         </div>
 
         <h2 className="text-2xl lg:text-3xl font-semibold text-neutral-400 py-6 px-1">
-          Your Lists 🛠
+          Your Lists
         </h2>
+        <div>
+          {Data.length > 0 ? (
+            <div className="grid lg:grid-cols-2 lg:gap-3 gap-3">
+              {Data.map((list, index) => (
+                <div
+                  key={index}
+                  className="shadow-md rounded-xl px-5 pt-4 bg-neutral-50"
+                >
+                  <h1 className="text-neutral-800 text-lg font-bold">
+                    {list.name}
+                  </h1>
+                  <h3 className="text-[11px] text-neutral-500 font-normal py-2">
+                    {list.description}
+                  </h3>
+                  <div className="flex justify-between py-4">
+                    <button
+                      className="flex items-center space-x-1 hover:scale-95 transition-all"
+                      onClick={() => {
+                        navigator.clipboard.writeText(`hit run ${list.name}`);
+                        toast({
+                          position: "top-right",
+                          render: () => (
+                            <Box color="white" py={3} px={5} bg="gray.800">
+                              Copied!
+                            </Box>
+                          ),
+                        });
+                      }}
+                    >
+                      <p>
+                        <IoCopyOutline size={17} />
+                      </p>
+                      <p className="text-xs font-medium">Copy</p>
+                    </button>
+
+                    <button className="flex items-center space-x-1 hover:scale-95 transition-all">
+                      <p>
+                        <BiEditAlt size={17} />
+                      </p>
+                      <p className="text-xs font-medium">Edit</p>
+                    </button>
+
+                    <button className="flex items-center space-x-1 text-red-500 hover:scale-95 transition-all">
+                      <p>
+                        <RiDeleteBin5Line size={17} />
+                      </p>
+                      <p className="text-xs font-medium">Delete</p>
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <h3 className="text-sm text-neutral-600 text-center font-semibold mt-[5vh]">
+              You have no lists.
+            </h3>
+          )}
+        </div>
       </main>
 
       <button className="fixed bottom-6 right-6 lg:bottom-10 lg:right-10 text-primary text-4xl p-3 bg-white rounded-full drop-shadow-lg">
