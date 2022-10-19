31864007dbf7f1494aea4d8ca1d38ed8ea9adb37
Ayodeji O <osasonaayodeji1@gmail.com>
2022-10-19 12:36:53 +0100

    Updated homepage

--- pages/docs/index.js
+++ pages/docs/index.js
@@ -13,45 +13,24 @@ const DocHome = () => {
         <HeaderText>Getting Started 🚀</HeaderText>
 
         <p className="text-sm mt-4 text-slate-600 px-1">
-          <span className="text-primary text-medium">Hit-List</span> is an
-          online commands manager that allows you to take your command lists
-          (i.e Hit Lists) with you anywhere and use them anytime. Hit-List CLI
-          tool is built to help you reduce time spent repeating commands in the
-          terminal; consequently improving your workflow efficiency.
-          <b className="block mt-2">
-            New: HitList can now save and sync your hits locally from the cloud.
-          </b>
+          <span className="text-primary text-medium">Hit-List</span> is an online commands manager that allows you to take your command lists (i.e Hit
+          Lists) with you anywhere and use them anytime. Hit-List CLI tool is built to help you reduce time spent repeating commands in the terminal;
+          consequently improving your workflow efficiency.
+          <b className="block mt-2">New: HitList can now save and sync your hits locally from the cloud.</b>
         </p>
-
-        <div className="flex justify-center mt-7">
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/wGCJQfiwTRA"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>
 
         <Info>
           <Text fontSize="xs">
-            Scenario 1 : You have 5 commands to start up your server/program
-            every day, that really does break the DRY (Don&apos;t Repeat
-            Yourself) rule. With{" "}
-            <span className="text-primary text-medium">Hit-List</span>, you can
-            run these commands with just one HIT on any computer.
+            Scenario 1 : You have 5 commands to start up your server/program every day, that really does break the DRY (Don&apos;t Repeat Yourself)
+            rule. With <span className="text-primary text-medium">Hit-List</span>, you can run these commands with just one HIT on any computer.
           </Text>
         </Info>
 
         <Info mt={5}>
           <Text fontSize="xs">
-            Scenario 2 : You are a very cautious coder and you commit your work
-            very often, but again you have to run 3/4 git commands; tsk, very
-            inefficient. This is already baked into{" "}
-            <span className="text-primary text-medium">Hit-List</span> and you
-            can easily run it with just ONE hit.
+            Scenario 2 : You are a very cautious coder and you commit your work very often, but again you have to run 3/4 git commands; tsk, very
+            inefficient. This is already baked into <span className="text-primary text-medium">Hit-List</span> and you can easily run it with just ONE
+            hit.
           </Text>
         </Info>
 
--- pages/index.js
+++ pages/index.js
@@ -46,24 +46,16 @@ const Home = () => {
 
   //GSAP
   useEffect(() => {
-    gsap
-      .fromTo(resOne.current, { opacity: 0 }, { opacity: 1, duration: 0.15 })
-      .delay(2.2);
+    gsap.fromTo(resOne.current, { opacity: 0 }, { opacity: 1, duration: 0.15 }).delay(2.2);
 
-    gsap
-      .fromTo(resTwo.current, { opacity: 0 }, { opacity: 1, duration: 0.15 })
-      .delay(3.3);
+    gsap.fromTo(resTwo.current, { opacity: 0 }, { opacity: 1, duration: 0.15 }).delay(3.3);
   }, []);
 
   return (
     <main className="Body">
       <Meta title="Hit-List CLI" />
-      <nav className="w-full bg-white fixed top-0 py-4 lg:py-5 px-5 lg:px-7 drop-shadow-md z-[99]">
-        <img
-          src="/images/Header-Black.svg"
-          alt="Header"
-          className="object-contain max-h-[4vh]"
-        />
+      <nav className="w-full bg-white fixed top-0 py-6 lg:py-6 px-5 lg:px-7 border-b border-b-neutral-100 z-[99]">
+        <img src="/images/Header-Black.svg" alt="Header" className="object-contain max-h-[4vh]" />
       </nav>
 
       <section className="w-[95%] md:w-[85%] lg:w-4/6 2xl:w-3/5 mx-auto pt-[15vh] lg:pt-[20vh]">
@@ -73,9 +65,9 @@ const Home = () => {
         <div className="w-[95%] mx-auto text-center text-[1.9rem] lg:text-[3.6rem] leading-normal lg:leading-relaxed text-neutral-800 lg:font-semibold font-bold">
           commands manager
         </div>
-        <h4 className="w-full mx-auto text-[12px] lg:text-sm font-medium text-center text-neutral-500 mt-[2vh] lg:mt-[3vh]">
-          Hit-List CLI allows you to carry your commands with you everywhere in
-          the cloud, share them with anyone, work smarter & faster.
+        <h4 className="w-full mx-auto text-[12px] lg:text-sm font-medium text-center text-neutral-500 mt-[2vh] lg:mt-[3vh] px-3">
+          With Hit-List CLI, you can bundle all your frequently used (or just annoyingly long) commands into one, save it in the cloud, make your list
+          accessible to anybody, and use them to work faster and smarter on any computer.
         </h4>
 
         {/* DOCS BUTTON */}
@@ -106,12 +98,8 @@ const Home = () => {
               <p className="text-neutral-400 text-xs lg:text-sm font-semibold">
                 $ <span className="text-white font-semibold" ref={One}></span>
               </p>
-              <span
-                className="text-xs lg:text-sm font-semibold text-white px-[1px]"
-                ref={resOne}
-              >
-                Currently logged in as{" "}
-                <span className="text-green-500 font-semibold">trulyao</span>
+              <span className="text-xs lg:text-sm font-semibold text-white px-[1px]" ref={resOne}>
+                Currently logged in as <span className="text-green-500 font-semibold">trulyao</span>
               </span>
             </div>
 
@@ -119,63 +107,37 @@ const Home = () => {
               <p className="text-neutral-400 text-xs lg:text-sm font-semibold">
                 $ <span className="text-white font-semibold" ref={Two}></span>
               </p>
-              <span
-                className="text-xs lg:text-sm font-semibold text-white px-[1px]"
-                ref={resTwo}
-              >
-                <span className="text-green-500 font-semibold">superlist</span>{" "}
-                has been successfully executed 🚀
+              <span className="text-xs lg:text-sm font-semibold text-white px-[1px]" ref={resTwo}>
+                <span className="text-green-500 font-semibold">superlist</span> has been successfully executed 🚀
               </span>
             </div>
           </div>
         </div>
 
         <div className="w-[95%] lg:w-full mx-auto pt-16 pb-24 bg-transparent grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-5">
-          <Feature image="Share">
-            Share your commands with anyone easily and quickly.
-          </Feature>
+          <Feature image="Share">Share your commands with anyone easily and quickly.</Feature>
           <Feature image="Hit">
-            Make your workflow faster and cleaner by running multiple commands
-            with one <b>hit</b>
+            Make your workflow faster and cleaner by running multiple commands with one <b>hit</b>
           </Feature>
-          <Feature image="Cloud">
-            Keep your lists safely in the cloud and use them anytime even
-            without an internet connection.
-          </Feature>
-          <Feature image="Doc">
-            Open-source codebase and a detailed usage documentation for users.
-          </Feature>
+          <Feature image="Cloud">Keep your lists safely in the cloud and use them anytime even without an internet connection.</Feature>
+          <Feature image="Doc">Open-source codebase and a detailed usage documentation for users.</Feature>
         </div>
       </section>
       <footer className="w-full bg-neutral-50 py-6 lg:py-10 px-6 lg:px-6">
         <div className="lg:w-4/5 flex items-center justify-between mx-auto">
           <div className="flex flex-col items-start space-y-3">
-            <img
-              src="/images/Header-Black.svg"
-              alt="Header"
-              className="object-contain h-6 lg:h-8"
-            />
-            <h4 className="lg:text-[11px] text-[10px] font-normal">
-              &copy;{currentDate.getFullYear()}. Your Online Commands Manager
-            </h4>
+            <img src="/images/Header-Black.svg" alt="Header" className="object-contain h-6 lg:h-8" />
+            <h4 className="lg:text-[11px] text-[10px] font-normal">&copy;{currentDate.getFullYear()}. Your Online Commands Manager</h4>
           </div>
 
           <div className="flex gap-2 lg:gap-3">
             <div className="bg-primary text-white p-2 rounded-full">
-              <a
-                href="https://twitter.com/hitlistcli"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              <a href="https://twitter.com/hitlistcli" target="_blank" rel="noopener noreferrer">
                 <FaTwitter size={15} />
               </a>
             </div>
             <div className="bg-neutral-700 text-white p-2 rounded-full">
-              <a
-                href="https://github.com/hitlist-cli/cli"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              <a href="https://github.com/hitlist-cli/cli" target="_blank" rel="noopener noreferrer">
                 <FaGithub size={15} />
               </a>
             </div>
